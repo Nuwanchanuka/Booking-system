1f7865977a18373a5edd3222d50cc5d768a77572
Nuwan Chanuka <140190472+Nuwanchanuka@users.noreply.github.com>
2025-01-05 09:35:46 +0530

    create AppContext file

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+ import {createContext } from 'react';
+ import { doctors } from '../assets/assets';
 
-const AppContext = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+ export const AppContext = createContext()
+
+ const AppContextProvider = (props) =>{
+    
+
+    const value = {
+      doctors  
+ }
+ return(
+    <AppContext.Provider value={value}>
+        {props.children}
+    </AppContext.Provider>
+ )
 }
-
-export default AppContext
+export default AppContextProvider
