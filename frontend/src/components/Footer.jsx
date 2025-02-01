6b155b66f20abd575e04eb871bc39d86ad734eac
Nuwan Chanuka <140190472+Nuwanchanuka@users.noreply.github.com>
2025-02-01 10:29:03 +0530

    create  footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,14 +7,14 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         {/* -----Left Side----- */}
         <div>
-             <img src={assets.logo} alt=""></img>
-             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+             <img className='mb-5 w-40' src={assets.logo} alt=""></img>
+             <p className='w-full md:w-2/3 text-gray-600 leading-6'> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         </div>
 
         {/* -----Center----- */}
         <div>
-            <p>COMPANY</p>
-            <ul>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
                 <li>Home</li>
                 <li>About us</li>
                 <li>Contact us</li>
@@ -27,8 +27,8 @@ const Footer = () => {
 
         {/* -----Right Side----- */}
         <div>
-           <p>GET IN TOUCH</p>
-              <ul>
+           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+              <ul className='flex flex-col gap-2 text-gray-600'>
                 <li>0717511510</li>
                 <li>chanukanuwan12345@gmail.com</li>
               </ul>  
